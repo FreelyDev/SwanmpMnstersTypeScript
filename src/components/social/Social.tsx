352de8a1b74d2372060fb2e1d6ec82f581028162
XuJiqing1988 <passion.dev@hotmail.com>
2021-09-25 21:41:06 +0800

    Complete type script

---
+++ src/components/social/Social.tsx
@@ -0,0 +1,13 @@
+import "./social.scss"
+import {Twitter} from "@material-ui/icons";
+
+export default function Social() {
+    return (
+        <div className="social">
+            <div className="socialIcon">
+                <a href="https://twitter.com/@swampmnstrsnft"><Twitter /></a>
+            </div>
+            
+        </div>
+    )
+}
